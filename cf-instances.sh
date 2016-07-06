e27c114c3a8134a9fd5d5a944fee75165cef009e
Stanislav German-Evtushenko <s.germanevtushenko@rakuten.com>
2016-07-06 12:38:30 +0900

    Add cf-instances.sh
    Usage: cf-instances.sh "application name"

---
+++ cf-instances.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+APP="$1"
+
+GUIDS=$(cf curl "/v2/apps?q=name%20IN%20${APP}" | jq -r '.resources[].metadata.guid')
+
+for guid in $GUIDS; do
+    echo "$APP ($guid)"
+    ( cf curl "/v2/apps/$guid/stats" | \
+        jq -r '["Index", "IP", "Port"], ( keys[] as $key | [$key, .[$key].stats.host, .[$key].stats.port] ) | @tsv' 2>/dev/null || \
+        echo -e '-\t-\t-'
+    ) | column -ets$'\t'
+    echo
+done
