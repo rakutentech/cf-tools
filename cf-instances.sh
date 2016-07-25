c3f93bebb10dc977be79cff0e94b4467f20bd37b
Stanislav German-Evtushenko <s.germanevtushenko@rakuten.com>
2016-07-25 17:14:59 +0900

    cf-instances.sh: Add "State" column
    1) Add State column to the output
    2) Cosmetic changes to the output
    3) Cosmetic changes to the code (formatting)

--- cf-instances.sh
+++ cf-instances.sh
@@ -6,11 +6,18 @@ APP="$1"
 
 GUIDS=$(cf curl "/v2/apps?q=name:${APP}" | jq -r '.resources[].metadata.guid')
 
+nl=false
 for guid in $GUIDS; do
-    echo "$APP ($guid)"
+    $nl && echo -ne "\n" || nl=true
+    echo "Application: $APP ($guid)"
     ( cf curl "/v2/apps/$guid/stats" | \
-        jq -r '["Index", "IP", "Port"], ( keys[] as $key | [$key, .[$key].stats.host, .[$key].stats.port] ) | @tsv' 2>/dev/null || \
-        echo -e '-\t-\t-'
+        jq -r '["Index", "State", "IP", "Port"],
+               ( keys[] as $key |
+                 [ $key, .[$key].state, .[$key].stats.host, .[$key].stats.port |
+                   select (. == null) = "<null>" |
+                   select (. == "") = "<empty>"
+                 ]
+               ) | @tsv' 2>/dev/null || \
+        echo -e '-\t-\t-\t-'
     ) | column -ets$'\t'
-    echo
 done
